a6c64904d98b70f5c930b42d6b37a8c8045967e6
alexsylvian <lazeratus1@gmail.com>
2024-05-22 22:21:40 -0400

    search function added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ import ProjectForm from '../components/ProjectForm';
 
 function Home() {
   const [projects, setProjects] = useState([]);
+  const [searchedProjects, setSearchedProjects] = useState('');
 
   useEffect(() => {
     fetch('/projects')
@@ -17,15 +18,31 @@ function Home() {
     setProjects([...projects, newProject]);
   }
 
+  function handleSearch(e) {
+    setSearchedProjects(e.target.value);
+  }
+
+  // Filter projects based on search query
+  const filteredProjects = projects.filter(project =>
+    project.name.toLowerCase().includes(searchedProjects.toLowerCase())
+  );
+
+
   return (
     <div>
-        <NavBar />
+      <NavBar />
       <h1>Welcome to Your Task Manager!</h1>
       <div>
+        <input
+          type="text"
+          placeholder="Search projects by name"
+          value={searchedProjects}
+          onChange={handleSearch}
+        />
         <h2>Projects</h2>
         <div className="project-cards">
           {/* Render ProjectCard components for each project */}
-          {projects.map((project) => (
+          {filteredProjects.map((project) => (
             <ProjectCard key={project.id} title={project.name} id={project.id} subtasks={project.subtasks} />
           ))}
         </div>
